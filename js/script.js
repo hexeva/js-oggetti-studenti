a823ff8aa11ce898b186c9366b447ce77e21f908
hexeva <patrick79.g@gmail.com>
2021-11-25 15:21:06 +0100

    created array of student

--- js/script.js
+++ js/script.js
@@ -12,17 +12,19 @@ const studentInfo ={
     cognome:'Galasso',
     eta:42
 };
+
+
 // mi restituisce tutto l'object con chiave e valore
-console.log(studentInfo);
+// console.log(studentInfo);
 // mi restituisce tramite dot-notation il valore (in questo caso, di età cioè il numero 42)
-console.log(studentInfo.eta);
+// console.log(studentInfo.eta);
 // mi restituisce tramite braket-notation il valore (in questo caso, di età cioè il numero 42)
-console.log(studentInfo['eta']);
+// console.log(studentInfo['eta']);
 
 //2---- Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 
 for (let key in studentInfo){
-    console.log(key + ':' + studentInfo[key]);
+    // console.log(key + ':' + studentInfo[key]);
     // console.log( studentInfo[key]);
 };
 
@@ -35,25 +37,47 @@ const studentArray = [
     {
         nome: 'Cinzia',
         cognome: 'Bianchi',
-        età: 30
+        eta: 30
     },
 
     {
         nome: 'Ettorino',
         cognome: 'Rossi',
-        età: 27
+        eta: 27
     },
 
     {
         nome: 'Gino',
         cognome: 'Verdi',
-        età: 20 
+        eta: 20 
     },
 
 
 
 ];
 
-console.log(studentArray);
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+
+// scorro tutto l'array studenti con un ciclo for
+
+for (let i = 0; i < studentArray.length; i++){
+    
+    // mi salvo in una variabile object
+    let studentObject = studentArray[i];
+    console.log(studentObject);
+    // stampare per ognuno di essi, nome e cognome con un ciclo for in
+
+    for (let key in studentObject){
+        // all'interno del ciclo for in con un if posso escludere il parametro età
+        if(key !== 'eta'){
+            console.log(studentObject[key]);
+        }
+
+    }
+    
+};
+
+
+
 
 
